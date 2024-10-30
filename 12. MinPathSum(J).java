08dba445d8ed326e3dc259d6576a14e6eda9bb6b
NirmalsaiswaroopJ <128392324+NirmalsaiswaroopJ@users.noreply.github.com>
2024-10-30 06:54:46 +0530

    Create 12. MinPathSum(J).java

---
+++ 12. MinPathSum(J).java
@@ -0,0 +1,27 @@
+public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        
+        dp[0][0] = grid[0][0];
+        
+        // Initialize the first row
+        for(int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        
+        // Initialize the first column
+        for(int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                int left = grid[i][j] + dp[i][j - 1];
+                int up = grid[i][j] + dp[i - 1][j];
+                dp[i][j] = Math.min(left, up);
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+}
